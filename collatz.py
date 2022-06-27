65ee308ff46522d522aed8b71928ce947e187cf9
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-06-27 13:34:53 +0600

    Create collatz.py

---
+++ collatz.py
@@ -0,0 +1,10 @@
+def collatz(initial_num):
+  while initial_num != 1:
+    if initial_num%2==0:
+      initial_num = initial_num/2
+      print(initial_num)
+    else:
+      initial_num = 3*initial_num +1 
+      print(initial_num)
+      
+collatz(7)
