ee874995fa3465125f5551cec960e0b904739c27
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-06-28 11:23:50 +0600

    outputs datatype converted to int

--- collatz.py
+++ collatz.py
@@ -1,7 +1,7 @@
 def collatz(initial_num):
   while initial_num != 1:
     if initial_num%2==0:
-      initial_num = initial_num/2
+      initial_num = initial_num//2
       print(initial_num)
     else:
       initial_num = 3*initial_num +1 
