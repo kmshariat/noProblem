b784f87d861a608037a533128dbc0100f26f2440
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-06-28 15:55:53 +0600

    Update and rename multiplesOfmn.py to mulOfmn.py

--- mulOfmn.py
+++ mulOfmn.py
@@ -1,8 +1,7 @@
-#compute function returns the number of multiples of num1 and num2 between a certain limit
-def compute(num1, num2, limit):
+#mulOfmn function returns the number of multiples of two numbers m and n between a certain limit
+def mulofmn(num1, num2, limit):
     count_num1 = limit//num1
     count_num2 = limit//num2
     count_num1_num2 = limit//(num1*num2)
     count = count_num1+count_num2-count_num1_num2
     return count
-compute(2,5,10)
